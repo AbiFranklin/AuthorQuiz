fe2ed3dd37a2395c7fcab2bdfbbc03bcb30727ad
Abigayle Franklin <Abifranklin@gmail.com>
2019-11-30 13:37:20 -0600

    Initial commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap'
+
+function AuthorQuiz() {
+  return (
+    <div className="App">
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default AuthorQuiz;

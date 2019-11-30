44b53a6d0bdc09f6f4a8f1a680f89b9dd30b67ce
Abigayle Franklin <Abifranklin@gmail.com>
2019-11-30 14:43:25 -0600

    Testing set up

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap'
 
 function AuthorQuiz() {
   return (
     <div className="App">
-      <Button>Click</Button>
+      <h1>Author Quiz</h1>
     </div>
   );
 }
--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("AuthorQuiz Tests", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<AuthorQuiz />, div)
+  })
+})

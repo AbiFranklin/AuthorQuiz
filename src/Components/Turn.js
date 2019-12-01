c7378fe23151f23368436dd0ba140ead6db47d02
Abigayle Franklin <Abifranklin@gmail.com>
2019-12-01 12:51:08 -0600

    Random answers showing with one by author

--- src/Components/Turn.js
+++ src/Components/Turn.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Row, Col, Alert } from 'react-bootstrap'
 import {authors} from '../Components/data';
+import {shuffle} from 'underscore'
 
 function Turn () {
+    const randomauthor = Math.floor(Math.random() * authors.length)
+
+    function randombooks(){
+        const allBooks = authors.reduce(function(p, c, i) {
+            return p.concat(c.books)
+        }, []);
+
+        return shuffle(allBooks).slice(0, 3);
+    }
+
+    const answers = randombooks()
+    const randomauthorbook = shuffle(authors[randomauthor].books).slice(0,1);
+    answers.push(randomauthorbook[0])
 
     return (
     <Row className='turn'>
-        <Col sm={{ span: 4 }}>
-            <img src={authors[1].imgUrl} className='authorimage' alt='Author' />
+        <Col sm={{ span: 4 }} className='image'>
+            <img src={authors[randomauthor].imgUrl} className='authorimage' alt='Author' />
         </Col>
         <Col sm={{ span: 8 }}>
-            {authors[0].books.map((title) => <Alert key={title} className='answer' variant='secondary'>{title}</Alert>)}
+            {shuffle(answers).map((title) => <Alert key={title} className='answer' variant='secondary'>{title}</Alert>)}
         </Col>
     </Row>
     )
--- src/Components/data.js
+++ src/Components/data.js
@@ -1,82 +1,82 @@
 export const authors = [
     {
-        imgUrl: '../../public/img/wshakespear.jpg',
+        imgUrl: './img/wshakespear.jpg',
         books: ['The Tempest', 'King John', 'Macbeth', 'The Comedy of Errors', 'Twelfth Night']
     },
     {
-        imgUrl: '../../public/img/achristie.jpg',
+        imgUrl: './img/achristie.jpg',
         books: ['And Then There Was None', 'Murder is Easy', 'Lord Edgware Dies', 'Murder on the Orient Express', 'Three Act Tragedy']
     },
     {
-        imgUrl: '../../public/img/bcartland.jpg',
+        imgUrl: './img/bcartland.jpg',
         books: ['Sawdust', 'Open Wings', 'The Leaping Flame', 'Out of Reach', 'The Forgotten City']
     },
     {
-        imgUrl: '../../public/img/hrobbins.jpg',
+        imgUrl: './img/hrobbins.jpg',
         books: ['Never Love a Stranger', 'The Dream Merchants', 'The Carpetbaggers', 'Where Love Has Gone', 'The Adventurers']
     },
     {
-        imgUrl: '../../public/img/gsimenon.jpg',
+        imgUrl: './img/gsimenon.jpg',
         books: ['Tropic Moon', 'The Crime at Lock', 'The Hotel Majestic', 'The Man Who Watched the Trains Go By', 'The Engagement']
     },
     {
-        imgUrl: '../../public/img/eblyton.jpg',
+        imgUrl: './img/eblyton.jpg',
         books: ['Adventures of the Wishing-Chair', 'The Enchanted Wood', 'Child Whispers', 'The Rockingdown Mystery', 'Noddy Goes to Toyland']
     },
     {
-        imgUrl: '../../public/img/ssheldon.jpg',
+        imgUrl: './img/ssheldon.jpg',
         books: ['The Naked Face', 'Bloodline', 'Tell Me Your Dreams', 'The Sands of Time', 'Master of the Game']
     },
     {
-        imgUrl: '../../public/img/jkrowling.jpg',
+        imgUrl: './img/jkrowling.jpg',
         books: ['Harry Potter and the Chamber of Secrets', 'Harry Potter and the Deathly Hallows', 'Harry Potter and the Order of the Phoenix', 'The Casual Vacancy', 'The Silkworm']
     },
     {
-        imgUrl: '../../public/img/dseuss.jpg',
+        imgUrl: './img/dseuss.jpg',
         books: ['Thidwick the Big-Hearted Moose', 'If I Ran the Zoo', 'The Seven Lady Godivas', 'The Foot Book', 'The Lorax']
     },
     {
-        imgUrl: '../../public/img/ltolstoy.jpg',
+        imgUrl: './img/ltolstoy.jpg',
         books: ['War and Peace', 'Anna Karenina', 'Sevastopol Sketches', 'Family Happiness', 'Hadji Murad']
     },
     {
-        imgUrl: '../../public/img/jcollins.jpg',
+        imgUrl: './img/jcollins.jpg',
         books: ['The Stud', 'Hollywood Wives', 'Dangerous Kiss', 'Lady Boss', 'Poor Little Bitch Girl']
     },
     {
-        imgUrl: '../../public/img/halger.jpg',
+        imgUrl: './img/halger.jpg',
         books: ['Fair Harvard', 'Ragged Dick; or, Street Life in New York with the Bootblacks', 'The Store Boy', 'Forging Ahead', 'Making His Mark']
     },
     {
-        imgUrl: '../../public/img/rlstine.jpg',
+        imgUrl: './img/rlstine.jpg',
         books: ['Bozos on Patrol', 'Revenge R Us', 'Full Moon Fever', 'Trapped in Bat Wing Hall', 'The Prom Queen']
     },
     {
-        imgUrl: '../../public/img/nroberts.jpg',
+        imgUrl: './img/nroberts.jpg',
         books: ['Vengeance In Death', 'Sea Swept', 'Irish Thoroughbred', 'Conspiracy In Death', 'Loyalty In Death']
     },
     {
-        imgUrl: '../../public/img/sking.jpg',
+        imgUrl: './img/sking.jpg',
         books: ['Carrie', 'The Shining', 'Cujo', 'The Dark Half', 'Bag Of Bones']
     },
     {
-        imgUrl: '../../public/img/rdahl.jpg',
+        imgUrl: './img/rdahl.jpg',
         books: ['Matilda', 'The Witches', 'Charlie and the Chocolate Factory', 'The BFG', 'James and the Giant Peach']
     },
     {
-        imgUrl: '../../public/img/jgrisham.jpg',
+        imgUrl: './img/jgrisham.jpg',
         books: ['The Pelican Brief', 'The Runaway Jury', 'Skipping Christmas', 'The Client', 'The Rainmaker']
     },
     {
-        imgUrl: '../../public/img/jrrtolkien.jpg',
+        imgUrl: './img/jrrtolkien.jpg',
         books: ['The Hobbit', 'The Lord of the Rings', 'On Fairy-Stories', 'The Silmarillion', 'Mr. Bliss']
     },
     {
-        imgUrl: '../../public/img/bpotter.jpg',
+        imgUrl: './img/bpotter.jpg',
         books: ['The Tale of Peter Rabbit', 'The Tailor of Gloucester', 'The Sly Old Cat', 'The Tale of the Flopsy Bunnies', 'The Fairy Caravan']
     },
     {
-        imgUrl: '../../public/img/dmacomber.jpg',
+        imgUrl: './img/dmacomber.jpg',
         books: ['A Good Yarn', 'The Shop on Blossom Street', 'Knit Together', 'Love Letters', '16 Lighthouse Road']
     },
    

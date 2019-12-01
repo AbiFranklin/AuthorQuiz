39ea6b3f2b55824fb75b8f4f021a01896d0eacac
Abigayle Franklin <Abifranklin@gmail.com>
2019-12-01 11:16:08 -0600

    Images and helper file in place

--- src/Components/Turn.js
+++ src/Components/Turn.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Row, Col, Alert } from 'react-bootstrap'
+import {authors} from '../Components/data';
 
 function Turn () {
+
     return (
-    <div>
-        Turn
-    </div>
+    <Row className='turn'>
+        <Col sm={{ span: 4 }}>
+            <img src={authors[1].imgUrl} className='authorimage' alt='Author' />
+        </Col>
+        <Col sm={{ span: 8 }}>
+            {authors[0].books.map((title) => <Alert key={title} className='answer' variant='secondary'>{title}</Alert>)}
+        </Col>
+    </Row>
     )
   }
 
